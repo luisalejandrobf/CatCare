94defb804646fcd9e84592e9a044a9a702aa5232
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2023-09-06 01:54:09 -0500

    Coevaluación added.

--- project/src/main/java/com/catcare/project/Controller/Error/ErrorController.java
+++ project/src/main/java/com/catcare/project/Controller/Error/ErrorController.java
@@ -3,6 +3,7 @@ package com.catcare.project.Controller.Error;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ErrorController {
