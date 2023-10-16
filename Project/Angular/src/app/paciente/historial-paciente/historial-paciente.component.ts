6a31c6817f7df7a3e40983d7322ffeb7f278b3e2
Arsete <anaortegon2003@gmail.com>
2023-10-16 01:12:29 -0500

    Adición botón eliminar

--- Project/Angular/src/app/paciente/historial-paciente/historial-paciente.component.ts
+++ Project/Angular/src/app/paciente/historial-paciente/historial-paciente.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { PacienteService } from "../../service/paciente/paciente.service";
 import {Paciente} from "../paciente";
+import {TratamientoService} from "../../service/tratamiento/tratamiento.service";
+import {Tratamiento} from "../../tratamiento/tratamiento";
 
 @Component({
   selector: 'app-historial-paciente',
@@ -11,7 +13,11 @@ import {Paciente} from "../paciente";
 export class HistorialPacienteComponent implements OnInit {
   tratamientos: any[] = [];
   @Input() paciente: Paciente | null = null;
-  constructor(private route: ActivatedRoute, private pacienteService: PacienteService) {}
+  constructor(
+      private route: ActivatedRoute,
+      private pacienteService: PacienteService,
+      private tratamientoService: TratamientoService
+  ) {}
 
   ngOnInit() {
     const pacienteId = this.route.snapshot.params['id'];
@@ -23,13 +29,21 @@ export class HistorialPacienteComponent implements OnInit {
       console.log('Paciente con ID', pacienteId, ':', paciente);
     });
 
-
-
-
     // Carga los tratamientos del paciente usando el ID
     this.pacienteService.getTratamientosByPacienteId(pacienteId).subscribe(tratamientos => {
       this.tratamientos = tratamientos;
       console.log('Tratamientos para el paciente con ID', pacienteId, ':', tratamientos);
+    });
+  }
+
+
+
+  eliminarTratamiento(tratamiento: Tratamiento) {
+    this.tratamientoService.eliminarTratamiento(tratamiento.id).subscribe(response => {
+      console.log('Respuesta al eliminar tratamiento con ID', tratamiento.id, ':', response);
+      window.location.reload(); // Recarga la página (o podrías querer actualizar la lista sin recargar la página)
+    }, error => {
+      console.error('Hubo un error al eliminar el tratamiento:', error);
     });
   }
 }
