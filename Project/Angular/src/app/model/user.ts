12d1782b4d3d8a64e4c1014b7da73ec575e356c4
Arsete <anaortegon2003@gmail.com>
2023-11-17 17:36:36 -0500

    Somos bobos jajajaa

--- Project/Angular/src/app/model/user.ts
+++ Project/Angular/src/app/model/user.ts
@@ -1,4 +1,4 @@
 export interface User {
   cedula: string;
-  password: string;
+  contrasena: string;
 }
--- Project/Angular/src/app/service/veterinario/veterinario.service.ts
+++ Project/Angular/src/app/service/veterinario/veterinario.service.ts
@@ -56,7 +56,7 @@ export class VeterinarioService {
   }
 
   // Verificar inicio de sesión de un veterinario
-  verificarInicioSesion(user: Veterinario): Observable<string> {
+  verificarInicioSesion(user: User): Observable<string> {
     return this.http.post(`${this.ROOT_URL}/login`, user,
       {
         responseType: "text"
