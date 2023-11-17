9ddb4e0d198827d19239e9437d514c4d8c07f29d
Arsete <anaortegon2003@gmail.com>
2023-11-16 21:42:17 -0500

    pruebas Queries modificadas

--- Project/SpringBoot/src/test/java/com/catcare/project/query/QueryTests.java
+++ Project/SpringBoot/src/test/java/com/catcare/project/query/QueryTests.java
@@ -6,40 +6,44 @@ import com.catcare.project.Entity.Tratamiento;
 import com.catcare.project.Entity.Veterinario;
 import com.catcare.project.Repository.DrogaRepository;
 import com.catcare.project.Repository.PacienteRepository;
+import com.catcare.project.Repository.TratamientoRepository;
 import com.catcare.project.Repository.VeterinarioRepository;
 import com.catcare.project.Service.DrogaService;
 import com.catcare.project.Service.TratamientoService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class QueryTests {
 
-    @Autowired
-    private TratamientoService tratamientoService;
-    
-    @Autowired
-    private DrogaService drogaService;
+    @Mock
+    private TratamientoRepository tratamientoRepository;
 
-    @Autowired
+    @Mock
     private PacienteRepository pacienteRepository;
 
-    @Autowired
+    @Mock
     private DrogaRepository drogaRepository;
 
-    @Autowired
+    @Mock
     private VeterinarioRepository veterinarioRepository;
 
     @BeforeEach
@@ -77,82 +81,91 @@ public class QueryTests {
         tratamientoRecent1.setDroga(droga1);
         tratamientoRecent1.setVeterinario(veterinarioTest);
         tratamientoRecent1.setFechaDeInicio(Date.valueOf(LocalDate.now().minusDays(10)));
-        tratamientoService.add(tratamientoRecent1);
+        tratamientoRepository.save(tratamientoRecent1);
 
         Tratamiento tratamientoRecent2 = new Tratamiento();
         tratamientoRecent2.setPaciente(pacienteTest);
         tratamientoRecent2.setDroga(droga2);
         tratamientoRecent2.setVeterinario(veterinarioTest);
         tratamientoRecent2.setFechaDeInicio(Date.valueOf(LocalDate.now().minusDays(5)));
-        tratamientoService.add(tratamientoRecent2);
+        tratamientoRepository.save(tratamientoRecent2);
 
         Tratamiento tratamientoRecent3 = new Tratamiento();
         tratamientoRecent3.setPaciente(pacienteTest);
         tratamientoRecent3.setDroga(droga3);
         tratamientoRecent3.setVeterinario(veterinarioTest);
         tratamientoRecent3.setFechaDeInicio(Date.valueOf(LocalDate.now().minusDays(15)));
-        tratamientoService.add(tratamientoRecent3);
+        tratamientoRepository.save(tratamientoRecent3);
 
         Tratamiento tratamientoRecent4 = new Tratamiento();
         tratamientoRecent4.setPaciente(pacienteTest);
         tratamientoRecent4.setDroga(droga4);
         tratamientoRecent4.setVeterinario(veterinarioTest);
         tratamientoRecent4.setFechaDeInicio(Date.valueOf(LocalDate.now().minusDays(8)));
-        tratamientoService.add(tratamientoRecent4);
+        tratamientoRepository.save(tratamientoRecent4);
 
         Tratamiento tratamientoRecent5 = new Tratamiento();
         tratamientoRecent5.setPaciente(anotherPacienteTest);
         tratamientoRecent5.setDroga(droga5);
         tratamientoRecent5.setVeterinario(veterinarioTest);
         tratamientoRecent5.setFechaDeInicio(Date.valueOf(LocalDate.now().minusDays(20)));
-        tratamientoService.add(tratamientoRecent5);
+        tratamientoRepository.save(tratamientoRecent5);
 
         Tratamiento inactiveTratamiento = new Tratamiento();
         inactiveTratamiento.setPaciente(anotherPacienteTest);
         inactiveTratamiento.setDroga(droga1);
         inactiveTratamiento.setVeterinario(veterinarioTest);
         inactiveTratamiento.setFechaDeInicio(Date.valueOf(LocalDate.now().minusMonths(3)));
-        tratamientoService.add(inactiveTratamiento);
+        tratamientoRepository.save(inactiveTratamiento);
     }
 
-    // 1. Cantidad total de tratamiento administrados en el último mes -> TratamientoService
+    // 1. Cantidad total de tratamiento administrados en el último mes -> DrogaRepository
+    
+        @Test
+        public void testCountTratamientosLastMonth() {
+            when(tratamientoRepository.countTratamientosLastMonth()).thenReturn(5L);
+            Long count = tratamientoRepository.countTratamientosLastMonth();
+            assertThat(count).isEqualTo(5L);
+        }
+    // 2. Cantidad de mascotas activas (en tratamiento) en la veterinaria -> DrogaRepository
+    
     @Test
-    public void countTratamientosLastMonth_ShouldReturnNumberOfTreatmentsInLastMonth() {
-        Long count = tratamientoService.countTratamientosLastMonth();
-        Assertions.assertThat(count).isEqualTo(5);
+    public void testCountMascotasEnTratamiento() {
+        when(tratamientoRepository.countMascotasEnTratamiento()).thenReturn(2L);
+        Long count = tratamientoRepository.countMascotasEnTratamiento();
+        Assertions.assertThat(count).isNotNull();
+        Assertions.assertThat(count).isEqualTo(2L);
     }
 
-    // 2. Cantidad de mascotas activas (en tratamiento) en la veterinaria -> TratamientoService
+    // 3. Ventas totales de la veterinaria -> DrogaRepository
+    
+
     @Test
-    public void countMascotasEnTratamiento_ShouldReturnNumberOfActivePets() {
-        Long count = tratamientoService.countMascotasEnTratamiento();
-        Assertions.assertThat(count).isEqualTo(2);
+    public void testCalcularVentasTotales() {
+        when(drogaRepository.calcularVentasTotales()).thenReturn(4950.0f);
+        Float ventasTotales = drogaRepository.calcularVentasTotales();
+        assertThat(ventasTotales).isEqualTo(4950.0f);
     }
-
-    // 3. Ventas totales de la veterinaria -> DrogaService
+    
+    // 4. Ganancias totales de la veterinaria -> DrogaRepository
     @Test
-    public void ventasTotales_ShouldReturnTotalSales() {
-        Float ventas = drogaService.calcularVentasTotales();
-        Float expectedVentas = 150.0f * 10 + 80.0f * 20 + 60.0f * 15 + 30.0f * 5 + 100.0f * 8;
-        Assertions.assertThat(ventas).isEqualTo(expectedVentas);
+    public void testCalcularGananciasTotales() {
+        when(drogaRepository.calcularGananciasTotales()).thenReturn(2170.0f);
+        Float gananciasTotales = drogaRepository.calcularGananciasTotales();
+        assertThat(gananciasTotales).isEqualTo(2170.0f);
     }
-
-    // 4. Ganancias totales de la veterinaria -> DrogaService
+    
+    // 5. Top 3 tratamientos con más unidades vendidas -> DrogaRepository
     @Test
-    public void gananciasTotales_ShouldReturnTotalEarnings() {
-        Float ganancias = drogaService.calcularGananciasTotales();
-        Float expectedGanancias = (150.0f - 100.0f) * 10 + (80.0f - 40.0f) * 20 + (60.0f - 30.0f) * 15 + (30.0f - 10.0f) * 5 + (100.0f - 60.0f) * 8;
-        Assertions.assertThat(ganancias).isEqualTo(expectedGanancias);
+    public void testGetTop3MedicamentosUnidadesVendidas() {
+        List<Object[]> datosEsperados = new ArrayList<>();
+        datosEsperados.add(new Object[]{"Antibiótico", 10L});
+        datosEsperados.add(new Object[]{"Antiparasitario", 20L});
+        datosEsperados.add(new Object[]{"Antiinflamatorio", 15L});
+        when(drogaRepository.getTop3MedicamentosUnidadesVendidas()).thenReturn(datosEsperados);
+
+        List<Object[]> resultado = drogaRepository.getTop3MedicamentosUnidadesVendidas();
+        assertThat(resultado).isEqualTo(datosEsperados);
     }
 
-    // 5. Top 3 tratamientos con más unidades vendidas -> DrogaService
-    @Test
-    public void top3MedicamentosUnidadesVendidas_ShouldReturnTop3TratamientosWithAdditionalData() {
-        List<Object[]> topResults = drogaService.getTop3MedicamentosUnidadesVendidas();
-
-        Assertions.assertThat(topResults.size()).isLessThanOrEqualTo(3);
-        List<String> drugNames = topResults.stream().map(result -> (String) result[0]).collect(Collectors.toList());
-        List<String> expectedDrugNames = List.of("Antibiótico", "Antiparasitario", "Antiinflamatorio", "Vitaminas", "Antiinfeccioso");
-        Assertions.assertThat(drugNames).allMatch(expectedDrugNames::contains);
-    }
 }
